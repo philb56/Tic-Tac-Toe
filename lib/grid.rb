94a8249d97b68eadcb7c7c993af2c285e2a96d30
philb56 <phil.bugden@outlook.com>
2018-03-01 18:59:02 +0000

    Refactor validation in Grid to make it less complicated

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,7 @@
 class Grid
   GRIDSIZE = 3
   DOT = "."
+  VALID = 'valid'
   def initialize
     @grid = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
   end
@@ -12,15 +13,21 @@ class Grid
   end
 
   def play(x, y, move_type)
-    message = 'This is not a valid move - should be between 1,1 and 3,3'
-    return message if validate_move(x, y) != :valid
-    @grid[x - 1][y - 1] = move_type
+    message = validate_move(x, y)
+    @grid[x - 1][y - 1] = move_type if message == VALID
+    message
   end
 
   def validate_move(x, y)
-    if (x.is_a? Integer) && x >= 1 && x <= 3 &&
-       (y.is_a? Integer) && y >= 1 && y <= 3
-      return :valid
+    if invalid_coordinate(x) || invalid_coordinate(y)
+      return 'This is not a valid move - should be between 1,1 and 3,3'
     end
+    return VALID
+  end
+
+  private
+
+  def invalid_coordinate(coord)
+    !((coord.is_a? Integer) && coord >= 1 && coord <= 3)
   end
 end
